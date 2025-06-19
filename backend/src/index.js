bdcbd54e479dd69ee5920bb4f112a85e8ae17d9b
Piyush Bhatt <134905218+PiyushBhatt705@users.noreply.github.com>
2025-06-19 12:17:09 +0530

    First Commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -16,28 +14,29 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
-app.use(cookieParser());
+// CORS setup
 app.use(
   cors({
-  origin: [
-    "http://localhost:5173",
-    "https://backendforchattify.onrender.com"
-  ],
-  credentials: true
-})
-
+    origin: [
+      "http://localhost:5173",
+      "https://frontendforchattify.onrender.com"
+    ],
+    credentials: true
+  })
 );
+app.options("*", cors()); // optional
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
+  console.log("Server is running on PORT: " + PORT);
   connectDB();
 });
